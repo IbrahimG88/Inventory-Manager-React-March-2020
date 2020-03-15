83d821d7d1fe2787e22bec8db9fc90f0e1f9a122
IbrahimG88 <goharyibrahim@gmail.com>
2020-03-15 21:22:20 +0200

    Adding items in drawer

---
+++ src/Features/AddItem/AddItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddItem extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to add Item component!
+            </div>
+        )
+    }
+}
